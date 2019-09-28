ef6ca2275914e79194b206494caf47506484f30f
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2019-09-28 20:16:27 -0300

    Mais um pouco

---
+++ Registro Escolar/src/franciscoribeirosilva/com/registroescolar/Registro.java
@@ -0,0 +1,25 @@
+package franciscoribeirosilva.com.registroescolar;
+
+public class Registro {
+
+    
+    void registrarAluno(){
+        Aluno aluno = new Aluno("q", "as");
+    }
+    
+    void registraProfessor(){
+        
+    }
+    
+    void registraDiciplina(){
+        
+    }
+    
+    public static void main(String args []){
+        
+        
+        
+    }
+}
+
+
